137977998cbe0ab2bf7f0c23ecce26d30ece769f
Bruno Vasconcelos <brunojasgv@gmail.com>
2023-01-28 14:45:48 +0000

    set item to public

--- Sources/BrowsrFramework/Browsr.swift
+++ Sources/BrowsrFramework/Browsr.swift
@@ -10,24 +10,24 @@ import Foundation
 
 public struct Browsr {
     
-    public let model: Model? = nil
+    public let item: Item? = nil
     
     public init() {}
 
     @available(iOS 13.0.0, *)
-    public func fetchOrgs(page: Int) async throws -> [Model.Item] {
+    public func fetchOrgs(page: Int) async throws -> [Item] {
         let response: Model = try await request(apiRouter: .fetchOrg(page: page))
         return response.items
     }
     
     @available(iOS 13.0.0, *)
-    public func searchOrgs(name: String) async throws -> [Model.Item] {
+    public func searchOrgs(name: String) async throws -> [Item] {
         let response: Model = try await request(apiRouter: .searchOrg(name: name))
         return response.items
     }
     
     @available(iOS 13.0.0, *)
-    public func sortOrgs(page: Int, sortType: String) async throws -> [Model.Item] {
+    public func sortOrgs(page: Int, sortType: String) async throws -> [Item] {
         let response: Model = try await request(apiRouter: .sortOrg(page: page, sortType: sortType))
         return response.items
     }
